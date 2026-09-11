b569f20e13ed07cd2a4ff25ad18fa392ac1db01b
monishachelle-max <monishachelle@gmail.com>
2026-09-11 15:18:16 +0530

    Implement CO3 alarm and test program

--- src/main.c
+++ src/main.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
 
+/* Function provided by co3.c */
+void start_alarm(int seconds);
+
+int main(void)
+{
+    int seconds;
+
+    printf("========================================\n");
+    printf("          CO3 ALARM CLOCK TEST\n");
+    printf("========================================\n");
+
+    printf("Enter alarm duration in seconds: ");
+
+    if (scanf("%d", &seconds) != 1)
+    {
+        printf("Invalid input.\n");
+        return 1;
+    }
+
+    start_alarm(seconds);
+
+    return 0;
+}
